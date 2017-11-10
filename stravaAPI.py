b04956ead25ae56454a130c4170765bfdd2ab4a6
Nick <ngonzo95@gmail.com>
2017-11-10 13:42:09 -0800

    Added the getLatestRun fun function

--- stravaAPI.py
+++ stravaAPI.py
@@ -1,5 +1,6 @@
 import urllib2
 import json
+import time
 
 class StravaAPI:
 	"""This class acts as an interface between the rest of the program and the 
@@ -36,7 +37,7 @@ class StravaAPI:
 		#Get the activities from the strava api
 		activities = self._sendRequest('https://www.strava.com/api/v3/activities')
 
-		print activities[0]
+		print self._getTime(activities[8])
 
 		runIDs = []
 		self._addRunIDsToList(activities,runIDs)
@@ -47,8 +48,20 @@ class StravaAPI:
 			if (act['type'] == "Run" and 'start_latlng' in act and act['start_latlng'] != None):
 				runIDs.append(act['id'])
 
+	def _getTime(self, activity):
+		#Get the utc time from the activity formated in the way strava likes it
+		stravaTime = activity['start_date']
 
+		#Convert it to Epoch time so that we can use it in queries
+		#2017-11-09T16:31:00Z
+		pattern = '%Y-%m-%dT%H:%M:%SZ'
+		
+		#Convert into epoch Time
+		epochTime = int(time.mktime(time.strptime(stravaTime, pattern)))
 
+		return epochTime
+
+	#TODO Refactor code of next two methods to remove duplicate code
 	"""This function gets the last numRuns runIDS a user has done from the strava api.
 	This function returns a tuple which includes the timestamp of the latest activity done"""
 	def getLastNRunIDs(self, numRuns):
@@ -57,6 +70,7 @@ class StravaAPI:
 
 		runIDs = []
 
+		timeStamp = ""
 		#pages numbers start at 1
 		pageNum = 1
 
@@ -70,17 +84,72 @@ class StravaAPI:
 			#Add the run ids to the list 
 			self._addRunIDsToList(activities,runIDs)
 
-			#If this is the first page we want to 
+			#If this is the first page we want to save the time of the first
+			#activity to return to the user
+			if pageNum == 1:
+				timeStamp = self._getTime(activities[0])
+
+
 			#Increment the page number
 			pageNum += 1
 
 			#Limit the number of pages we can look at so we dont go on forever
-			#if they dont have enough runs
-			if pageNum > (numRuns/3):
+			#if they dont have enough runs. also if the last page was empty end
+			if pageNum > (numRuns/3) or activities == []:
 				break
 
+
 		#We may get more runs than we need so only return the amount they asked for
-		return runIDs[:numRuns]
+		return (timeStamp, runIDs[:numRuns])
+
+	"""Gets the all the runs since a given time. Optional argument to set a maximum on
+	the number of runs you want"""
+	def getLatestRuns(self, lastRunTime, maxRuns=100):
+		#Create base URL and initalize parameters
+		baseURL = 'https://www.strava.com/api/v3/activities?after=' +str(lastRunTime) + '&page='
+		pageNum = 1
+		runIDs = []
+		timeStamp = ''
+
+		#Grab the first page
+		url = baseURL + str(pageNum)
+		activities = self._sendRequest(url) 
+		
+		#If there have been no new activities since last time return 
+		if activities == []:
+			return(lastRunTime,[])
+
+		#Get the time of the latest activity
+		timeStamp = self._getTime(activities[0])
+
+		#While activities are not empty
+		while activities:
+			#Add the run ids to the list 
+			self._addRunIDsToList(activities,runIDs)
+
+			#Increment the page number
+			pageNum += 1
+
+			#Create the url to query and query it
+			url = baseURL + str(pageNum)
+			activities = self._sendRequest(url)
+
+			#If there are alot of runs exit once we hit the
+			#max
+			if len(runIDs) > maxRuns:
+				#Clean up the array so we only have the max
+				runIDs = runIDs[:maxRuns]
+				break
+
+			#if there are alot of pages that do not have runs exit
+			#so we dont accidentally overload the api
+			if pageNum > (maxRuns/3):
+				break
+		
+
+		return(timeStamp, runIDs)
+
+
 
 	""" This function takes in a run id and returns the runs gps information as
 	a list of gps position lists"""
@@ -107,9 +176,13 @@ class StravaAPI:
 #Basic script to show that class functions work as expected
 def main():
 	client = StravaAPI('/Users/Nick/Documents/stravaProject/stravaPicture/token.txt')
-	#runIDs = client.getLastNRunIDs(50)
+	#timeStamp, runIDs = client.getLastNRunIDs(50)
 
-	#runIDs = client.getRunIDs()
+	#runIDs = client._getRunIDs()
+	timeStamp, runIDs = client.getLatestRuns(1507575398)
+
+	print timeStamp
+	print len(runIDs)
 	
 	# gpsData = []
 	# for ID in runIDs:

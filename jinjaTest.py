2fac764218b29c24c1ce3462a3026d6ef70d0daf
Nick <ngonzo95@gmail.com>
2017-11-26 04:51:07 -0500

    moved dropdown code to template.html and made it so that its contents are filled in by parameters that are passed to it

--- jinjaTest.py
+++ jinjaTest.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
 	templateEnv = jinja2.Environment(loader=templateLoader)
 	TEMPLATE_FILE = "template.html"
 	template = templateEnv.get_template( TEMPLATE_FILE )
-	outputText = template.render() # this is where to put args to the template renderer
+	outputText = template.render({'maps':[{'href':'index.html', 'caption': 'hi'}, {'href':'#', 'caption': 'bye', 'current': True}]}) # this is where to put args to the template renderer
 
 	with open("my_new_file.html", "wb") as fh:
 		fh.write(outputText)

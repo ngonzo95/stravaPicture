9cc223e1ad9a7cf5c3d9d596e656494aabdc9185
Nick <ngonzo95@gmail.com>
2017-12-16 11:29:34 -0800

    preliminary edits of setup script.

--- shellScripts/setupScript.sh
+++ shellScripts/setupScript.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 #This script runs before Chromium is launched 
+#This script checks for internet connectivity. If there is no connnection
+#Open the wifi settings and time zone so the user can change them.
 
+if ping -c 1 asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfadfsadf.com >/dev/null ; then
+	echo hi
 
+else
+	echo Internet connectivity Confirmed, Doing nothing
+fi

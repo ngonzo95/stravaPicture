c82129319674b06904a3f0228bdda08a73150eed
Nick <ngonzo95@gmail.com>
2017-12-16 12:57:55 -0800

    added text for casewhere there is no internet.

--- shellScripts/setupScript.sh
+++ shellScripts/setupScript.sh
@@ -4,8 +4,11 @@
 #Open the wifi settings and time zone so the user can change them.
 
 if ping -c 1 asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfadfsadf.com >/dev/null ; then
-	echo hi
+	echo Internet connectivity confirmed. Do nothing.
 
 else
-	echo Internet connectivity Confirmed, Doing nothing
+	leafpad /home/pi/Documents/stravaPicture/noInternetText.txt & 
+	matchbox-keyboard &
+	sudo raspi-config
+	sudo leafpad /etc/wpa_supplicant/wpa_supplicant.conf
 fi

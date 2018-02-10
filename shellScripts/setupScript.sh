6e1f23db3bd9b9882f12abb1488b0a440c852b0e
Nick <ngonzo95@gmail.com>
2018-02-10 13:01:04 -0800

    Fixed setup script issues where it was taking a while to aquire internet.

--- shellScripts/setupScript.sh
+++ shellScripts/setupScript.sh
@@ -3,15 +3,23 @@
 #This script checks for internet connectivity. If there is no connnection
 #Open the wifi settings and time zone so the user can change them.
 
-if ping -c 1 google.com >/dev/null ; then
-	echo Internet connectivity confirmed. Starting Chromium.
-	chromium-browser --noerrdialogs --incognito --kiosk /home/pi/Documents/stravaPicture/views/index0.html
+i="0"
 
+while [ $i -lt 10 ]
+do
+	if ping -c 1 google.com >/dev/null ; then
+		echo Internet connectivity confirmed. Starting Chromium.
+		chromium-browser --noerrdialogs --incognito --kiosk /home/pi/Documents/stravaPicture/views/index0.html
+		exit
+	fi
 
-else
-	leafpad /home/pi/Documents/stravaPicture/noInternetText.txt & 
-	matchbox-keyboard &
-	sudo raspi-config
-	sudo leafpad /etc/wpa_supplicant/wpa_supplicant.conf
-        sudo reboot
-fi
+	sleep 2
+	i=$[$i+1]
+done
+
+leafpad /home/pi/Documents/stravaPicture/noInternetText.txt & 
+matchbox-keyboard &
+sudo raspi-config
+sudo leafpad /etc/wpa_supplicant/wpa_supplicant.conf
+    sudo reboot
+
